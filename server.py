058967e59af97c51554b67c56a296955af18a78f
Glitch (flask-python3) <none>
2021-06-10 08:31:32 +0000

    💅🍃 Checkpoint ./requirements.txt:4880197/17 ./server.py:4880197/394

--- server.py
+++ server.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
+  app.CommCareAPIKey = os.environ.get('CommCareAPIKey')
+  app.CommCareBaseURL = os.environ.get('CommCareBaseURL')
+
+  response = requests.get(
+      app.CommCareBaseURL + 'application/a1b787437bda83e6976f0706d46961ff',
+      headers={'Accept': 'application/json', 'Authorization': 'ApiKey ' + app.CommCareAPIKey }      
+  )
+  
+  
+
 if __name__ == "__main__":
   app.run()
