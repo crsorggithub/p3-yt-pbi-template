138a7014d128b61e176feeb1cbe8be457522745c
Glitch (flask-python3) <none>
2021-07-20 10:37:02 +0000

    🐋🌍 Checkpoint ./server.py:4880197/162

--- server.py
+++ server.py
@@ -23,8 +23,9 @@ def hello():
   print('-------')
   for element in questions:
     
-    if (not element["is_group"]) or element["type"] != "Trigger" or element["type"] is not "FieldList" :
-      print(element["type"] + ' ' + element["label"])
+    print (str(not element["is_group"]) + ' ' + str(element["type"] != "Trigger") + ' ' + str(element["type"] != "FieldList"))
+    if (not element["is_group"]) or (element["type"] != "Trigger" and element["type"] != "FieldList") :
+      print(element["type"] + ' - ' + element["label"])
       el = {}
     #el["Label"] = fixStr(element["label"])
     #el["LabelFR"] = fixStr(element["translations"]["en"])
