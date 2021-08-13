db54560c23880d60942421a0781fa0f2ddeee275
Glitch (flask-python3) <none>
2021-08-13 20:28:03 +0000

    🚛💝 Checkpoint ./server.py:4880197/26591

--- server.py
+++ server.py
@@ -17,15 +17,26 @@ def hello():
   app.YTToken = os.environ.get('YTtokenv3')
   app.YTGoldCopyURL = os.environ.get('YTGoldCopyURL')
 
-  
-  hdrs = {
-    'Accept': 'application/json', 
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + app.YTToken
-  }
+  hdrs = { 'Accept': 'application/json', 'Content-Type': 'application/json','Authorization': 'Bearer ' + app.YTToken}
 
+  # Add the new project to YouTdack
+  newProjectJson = {"shortName": "TXT001", "name": "TXT 001 Projects", "description": "A new project created from rest api", "leader":{"id":"1-1"}}
 
-  projectId = "0-34"  
+  # Create the project template
+  try:
+    response = requests.post(
+        app.YTGoldCopyURL + '/youtrack/api/admin/projects?fields=id,shortName,name,leader(id,login,name)',
+        headers=hdrs,
+        json = newProjectJson 
+    )
+    json_response = response.json()
+    print(json_response)
+   
+    projectId = json_response['id']
+  except requests.exceptions.RequestException as e:  # This is the correct syntax
+      raise SystemExit(e)   
+
+  #projectId = "0-34"  
   
   # DELETE THE FIELDS WE DONT NEED FIRST
   #first get the fields in the project that we dont need  
@@ -64,6 +75,38 @@ def hello():
   except requests.exceptions.RequestException as e:  # This is the correct syntax
       raise SystemExit(e)      
   
+
+
+    # Add the field to YouTrack
+  field = {"fieldType": {"id": "enum[1]"}, "name": "Gender", "isDisplayedInIssueList": True, "isAutoAttached": False, "isPublic": True }
+  try:
+    response = requests.post(
+        app.YTGoldCopyURL + '/youtrack/api/admin/customFieldSettings/customFields?fields=id,name,fieldType(presentation,id)',
+        headers=hdrs,
+        json = field 
+    )
+    json_response = response.json()
+    print(json_response)
+
+    fieldid = json_response['id']
+  except requests.exceptions.RequestException as e:  # This is the correct syntax
+      raise SystemExit(e)   
+
+    # Attach the field to the project
+  #field = {"field": {"ordinal": 8, "instances": [], "usages": [], "isAutoAttached": False, "fieldDefaults": {"canBeEmpty": True, "isPublic": True, "id": fieldid, "$type": "CustomFieldDefaults"}, "isUpdateable": True, "isDisplayedInIssueList": True, "fieldType": {"valueType": "text", "isMultiValue": False, "isBundleType": False, "id": "text", "$type": "FieldType"}, "hasRunningJob": False, "name": "Due Date", "id": fieldid, "$type": "CustomField"}, "$type": "SimpleProjectCustomField"}
+  field = {"field": {"id": fieldid}, "$type": "SingleEnumIssueCustomField"} 
+  try:
+    response = requests.post(
+        app.YTGoldCopyURL + '/youtrack/api/admin/projects/'+projectId+'/customFields?fields=id,name',
+        headers=hdrs,
+        json = field 
+    )
+    json_response = response.json()
+
+    fieldid = json_response['id']
+    print(json_response)
+  except requests.exceptions.RequestException as e:  # This is the correct syntax
+      raise SystemExit(e)   
 
   
   return "Hello World! " 
